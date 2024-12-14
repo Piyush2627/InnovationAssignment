458a39724e062370aeedaf75b142e8ae0cc010ea
Piyush Dahiwadkar <97776547+Piyush2627@users.noreply.github.com>
2024-12-14 15:17:17 +0530

    dependences added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import Routes from "./routes/Routes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 function App() {
   return (
     <>
-      <QueryClientProvider client={new QueryClient()}>
-        <Routes />
-      </QueryClientProvider>
+      <Routes />
     </>
   );
 }
